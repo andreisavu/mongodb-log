7f78b6bc529883f7b2631ccc0cb834e600343bf5
Andrei Savu <savu.andrei@gmail.com>
2009-09-05 10:06:36 +0300

    using only the mongo handler in the demo

--- samples/simple_logging.py
+++ samples/simple_logging.py
@@ -11,13 +11,6 @@ if __name__ == '__main__':
     log = logging.getLogger('example')
     log.setLevel(logging.DEBUG)
 
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.DEBUG)
-
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-    ch.setFormatter(formatter)
-
-    log.addHandler(ch)
     log.addHandler(MongoHandler())
 
     log.debug("debug message")
