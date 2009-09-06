c325c9ebb76d658aae183b566707b27b73ac92ec
Andrei Savu <savu.andrei@gmail.com>
2009-09-06 13:40:23 +0300

    simple setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils import setup
+    
+setup(
+      name='mongodb-log',
+      version='0.1.0',
+      description='Centralized logging made simple using mongodb',
+      author='Andrei Savu',
+      author_email='contact@andreisavu.ro',
+      url='http://github.com/andreisavu/mongodb-log/tree/master',
+      packages=['mongolog']
+)
