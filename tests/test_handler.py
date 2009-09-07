19b05c51afb358c1b25586f674a0724163aafc95
Andrei Savu <savu.andrei@gmail.com>
2009-09-07 16:56:32 +0300

    simple test for logging

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,15 +1,33 @@
 
 import unittest
+import logging
+
+from pymongo.connection import Connection
+
 from mongolog.handlers import MongoHandler
 
 class TestHandler(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.db_name = '_mongolog_test'
+
+        self.conn = Connection('localhost')
+        self.conn.drop_database(self.db_name)
+
+        self.db = self.conn[self.db_name]
+        self.collection = self.db['log']
 
     def tearDown(self):
-        pass
+        self.conn.drop_database('_mongolog_test')
+        
 
     def testLogging(self):
-        pass
+        log = logging.getLogger('example')
+        log.setLevel(logging.DEBUG)
 
+        log.addHandler(MongoHandler(self.collection))
+
+        log.debug('test')
+
+        r = self.collection.find_one({'level':'debug', 'msg':'test'})
+        self.assertEquals(r['msg'], 'test')
